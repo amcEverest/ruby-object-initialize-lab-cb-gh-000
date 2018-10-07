8dbe2288fc30d5121fc7e5bf9b7339efece4b721
Andres Cortez <andres.corteztm@gmail.com>
2018-10-07 20:28:50 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,10 +3,10 @@ class Dog
     @name = name
     if breed == nil
       breed = "Mutt"
-      return breed
       @breed = breed
     else
       @breed = breed
     end
+  return breed
   end
 end
